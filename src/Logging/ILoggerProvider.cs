72bc698923472c1cc1ad18870a4ce87944ba59f7
Городилов Роман (Gorodilov_RA) <Gorodilov_RA@directum.ru>
2021-05-18 02:08:09 +0400

    Добавил .editorconfig и изменил способ задания метода создания логгеров.

---
+++ src/Logging/ILoggerProvider.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Sungero.Plugins.Sdk.Logging
+{
+  /// <summary>
+  /// Интерфейс, для типа создающего логгеры.
+  /// </summary>
+  public interface ILoggerProvider : IDisposable
+  {
+    /// <summary>
+    /// Создать логгер.
+    /// </summary>
+    /// <param name="loggerName">Имя логера.</param>
+    /// <returns>Экземляр логгера.</returns>
+    ILog Create(string loggerName);
+  }
+}
